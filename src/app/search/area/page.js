be34abb7cf6d0184e44f8abf3e0087c5c2e7145a
popo26 <mynameispopo26@gmail.com>
2023-11-22 17:49:08 +1300

    react-world-flags added

--- src/app/search/area/page.js
+++ src/app/search/area/page.js
@@ -1,15 +1,49 @@
 "use client";
 import styles from "../../page.module.css";
 import Link from "next/link";
+import Flag from "react-world-flags";
 
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
-
-
+const flags = [
+  { American: "us" },
+  { British: "uk" },
+  { Canadian: "ca" },
+  { Chinese: "cn" },
+  { Croatian: "hr" },
+  { Dutch: "nl" },
+  { Egyptian: "eg" },
+  { Filipino: "ph" },
+  { French: "fr" },
+  { Greek: "gr" },
+  { Indian: "in" },
+  { Irish: "ie" },
+  { Italian: "it" },
+  { Jamaican: "jm" },
+  { Japanese: "jp" },
+  { Kenyan: "ke" },
+  { Malaysian: "my" },
+  { Mexican: "mx" },
+  { Moroccan: "ma" },
+  { Polish: "pl" },
+  { Portiguese: "pt" },
+  { Russian: "ru" },
+  { Spanish: "es" },
+  { Thai: "th" },
+  { Tunisian: "tn" },
+  { Turkish: "tr" },
+  { Unknown: "cn" },
+  { Vietnamese: "vn" },
+];
 
 //MUI - Grid
 const Item = styled(Paper)(({ theme }) => ({
@@ -31,8 +65,37 @@ async function getArea() {
   return res.json();
 }
 
+// async function searchImage(term) {
+//   // const response = await axios.get("https://api.unsplash.com/search/photos", {
+//   const response = await fetch(
+//     `https://api.unsplash.com/search/photos?page=1&query=${term}`,
+//     {
+//       headers: {
+//         Authorization: `Client-ID ${process.env.REACT_APP_API_ACCESS_KEY}`,
+//       },
+//       params: {
+//         query: term,
+//       },
+//     }
+//   );
+//   return response.data.results;
+// }
+
+async function searchFlag(term) {
+  const res = await fetch(
+    `https://api.unsplash.com/search/photos?client_id=${process.env.REACT_APP_API_ACCESS_KEY}&page=1&per_page=1&query=${term}-flag`
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch post #" + term);
+  }
+  return res.json();
+}
+
 export default async function Area() {
   const result = await getArea();
+
+  // const flags = await searchFlag("american");
+  // console.log(flags["results"][0].urls.small);
 
   return (
     // <main className={styles.main}>
@@ -55,9 +118,8 @@ export default async function Area() {
     // </main>
     <>
       <h1>Nationalities</h1>
-     
       <hr />
-      {result ? (
+      {/* {result ? (
         result.meals.map((item) => (
           <div key={item.strArea}>
             <Box sx={{ flexGrow: 1 }}>
@@ -69,6 +131,33 @@ export default async function Area() {
                 </Grid>
               </Grid>
             </Box>
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )} */}
+
+      {result ? (
+        result.meals.map((item) => (
+          <div key={item.strArea}>
+            <Link href={`/search/area/${item.strArea}`}>
+              <Card sx={{ maxWidth: 345 }}>
+                <Flag code={flags[item.strArea]} />
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={null}
+                    alt={item.strArea}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {item.strArea}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Link>
           </div>
         ))
       ) : (
